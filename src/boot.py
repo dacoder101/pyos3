936989db558ffca15f8996f21b6ef66d69ee4fcf
dacoder101 <107454678+dacoder101@users.noreply.github.com>
2023-06-02 23:48:49 +0000

    Added util.py and additions to boot

--- src/boot.py
+++ src/boot.py
@@ -1,1 +1,16 @@
+from func.file import create_file
 from func.path import path_exists
+from func.util import file_exec
+from func.dir import create_dir
+
+BLANKS_PATH = "store/blanks/"
+
+
+def install():
+    if not path_exists(BLANKS_PATH):
+        create_dir(BLANKS_PATH)
+        create_file(BLANKS_PATH)
+        file_exec("install.py")
+
+
+install()
---
+++ src/func/util.py
@@ -0,0 +1,5 @@
+from func.file import read_file
+
+
+def file_exec(path):
+    exec(read_file(path))
